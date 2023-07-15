8d90b1553f9da693e873f567267d670c0282caaf
Mayla Silva <maylajamile16@gmail.com>
2023-07-15 00:03:09 -0300

    corrigindo identação

--- src/desafio01/TesteDesafio01.java
+++ src/desafio01/TesteDesafio01.java
@@ -6,18 +6,18 @@ public class TesteDesafio01 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Digite a quantidade de números que será digitado:");
 		int quantNumeros = scanner.nextInt();
-		
-		int [] array = new int[quantNumeros];
+
+		int[] array = new int[quantNumeros];
 		for (int i = 0; i < quantNumeros; i++) {
 			System.out.println("Digite o número:");
 			array[i] = scanner.nextInt();
 		}
-		
+
 		new Desafio01().ordenarNumeros(array);
 		scanner.close();
-		
+
 	}
 }
--- src/desafio03/Desafio03.java
+++ src/desafio03/Desafio03.java
@@ -3,9 +3,9 @@ package desafio03;
 public class Desafio03 {
 
 	public void definirPares(int[] array, int valorAlvo) {
-		
+
 		int quantPares = 0;
-		
+
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array.length; j++) {
 				if ((array[i] - array[j]) == valorAlvo) {
@@ -13,7 +13,7 @@ public class Desafio03 {
 				}
 			}
 		}
-		
+
 		System.out.println("O número de pares que satisfazem o critério é: " + quantPares);
 	}
 
